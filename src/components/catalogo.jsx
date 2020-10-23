67c88e86818678e9afe9b9a092e5c895665ab45d
Lucas Carvalho <lucas.araripina@hotmail.com>
2020-10-23 16:15:32 -0300

    versão com o catalogo completo

--- src/components/catalogo.jsx
+++ src/components/catalogo.jsx
@@ -17,63 +17,172 @@ export default props =>
 						<div class="acessorio-img" ></div>
 						<div class="acessorio-info">
 							<h2>Colar</h2>
-							<h3>20,00 R$</h3>
-							<p>lorem ipsum sit door amet</p>
+							<h3>86,00 R$</h3>
+							<p>Colar Lua</p>
 							<a href="#/contatos">mais detalhes</a>
 						</div>
 					</div>
 					<div class="vitrine">
 						<div class="acessorio-img" ></div>
 						<div class="acessorio-info">
-							<h2>Anel</h2>
-							<h3>20,00 R$</h3>
-							<p>lorem ipsum sit door is amet</p>
+							<h2>Conjunto 3 aneis</h2>
+							<h3> 140,70R$</h3>
+							<p>Anel de falange corações 39,90R$<br></br>Anel de falange girl 55,80R$<br></br>falange coração Vazado 45,00R$</p>
 							<a href="#/contatos">mais detalhes</a>
 						</div>
 					</div>
 					<div class="vitrine">
 						<div class="acessorio-img"></div>
 						<div class="acessorio-info">
-							<h2>pulseira</h2>
-							<h3>20,00 R$</h3>
-							<p>lorem ipsum sit door is amet </p>
+							<h2> conjunto 2 pulseiras</h2>
+							<h3>108,00R$</h3>
+							<p>pulseira de coração 80,00R$<br></br>pulseira de elos 28,00R$</p>
 							<a href="#/contatos">mais detalhes</a>
 						</div>
 					</div>
 					<div class="vitrine">
 						<div class="acessorio-img" ></div>
 						<div class="acessorio-info">
-							<h2>pulseira de pé</h2>
-							<h3>20,00 R$</h3>
-							<p>Lorem ipsum sit door is amet</p>
+							<h2>tornozeleira</h2>
+							<h3>47,40R$</h3>
+							<p>Tornozeleira de corações</p>
 							<a href="#/contatos">mais detalhes</a>
 						</div>
 					</div>
 					<div class="vitrine">
 						<div class="acessorio-img" ></div>
 						<div class="acessorio-info">
-							<h2>Anel</h2>
-							<h3>20,00 R$</h3>
-							<p>Lorem ipsum sit door is amet</p>
+							<h2>Conjunto 3 aneis</h2>
+							<h3>115,00 R$</h3>
+							<p>anel de estrelinna 59,90R$<br></br>Anel solitário com zircôniaR$<br></br>Anel de coração vazado 34,90R$</p>
 							<a href="#/contatos">mais detalhes</a>
 						</div>
 					</div>
 					<div class="vitrine">
 						<div class="acessorio-img" ></div>
 						<div class="acessorio-info">
-							<h2>pulseira</h2>
-							<h3>20,00 R$</h3>
-							<p>Lorem ipsum sit door is amet</p>
+							<h2>pulseiras</h2>
+							<h3>103,90 R$</h3>
+							<p>pulseira de coração 18,5 cm 31,90R$<br></br>pulseira de elos 19 cm 34,00R$<br></br>pingente de cruz 38,00R$</p>
 							<a href="#/contatos">mais detalhes</a>
 						</div>
 					</div>
 					<div class="vitrine">
 						<div class="acessorio-img"></div>
 						<div class="acessorio-info">
-							<h2>colar</h2>
-							<h3>20,00 R$</h3>
-							<p>Lorem ipsum sit door is amet</p>
-							<a href="individual.html">mais detalhes</a>
+							<h2>colares</h2>
+							<h3>293,30 R$</h3>
+							<p>veneziana 40cm 34,00R$<br></br>Pingente Sou grata 44,50R$<br></br>colar fé 89,90R$<br></br>Veneziana 70cm 63,00R$<br></br>Pingente medalha de são Bento 61,90R$</p>
+							<a href="#/contatos">mais detalhes</a>
+						</div>
+					</div>
+					<div class="vitrine">
+						<div class="acessorio-img" ></div>
+						<div class="acessorio-info">
+							<h2>Anel</h2>
+							<h3>86,00 R$</h3>
+							<p>Colar lua</p>
+							<a href="#/contatos">mais detalhes</a>
+						</div>
+					</div>
+					<div class="vitrine">
+						<div class="acessorio-img" ></div>
+						<div class="acessorio-info">
+							<h2>Brincos conjunto</h2>
+							<h3>156,00 R$</h3>
+							<p>brinco de pressão zircônia 44,90<br></br>argola pequena 44,60<br></br>argola triângulo 66,50</p>
+							<a href="#/contatos">mais detalhes</a>
+						</div>
+					</div>
+					<div class="vitrine">
+						<div class="acessorio-img" ></div>
+						<div class="acessorio-info">
+							<h2>Brincos</h2>
+							<h3> 39,90R$</h3>
+							<p>Brinco Trio de prata</p>
+							<a href="#/contatos">mais detalhes</a>
+						</div>
+					</div>
+					<div class="vitrine">
+						<div class="acessorio-img" ></div>
+						<div class="acessorio-info">
+							<h2>Brincos</h2>
+							<h3>222,90 R$</h3>
+							<p>Brinco de coração vazado 54,00<br></br>Piercing de pressão 44,00<br></br>Brinco folha 42,00<br></br>Brinco flor com zircônia 49,90<br></br>Brinco pena 33,00
+</p>
+							<a href="#/contatos">mais detalhes</a>
+						</div>
+					</div>
+
+					<div class="vitrine">
+						<div class="acessorio-img" ></div>
+						<div class="acessorio-info">
+							<h2>Brincos</h2>
+							<h3>124,00 R$</h3>
+							<p>Brinco zircônia 28,00<br></br>Brinco zircônia menor 22,00<br></br>Brinco de coração vazado 38,00<br></br>Brinco de estrela 36,00<br></br>Brinco círculo vazado 49,90</p>
+							<a href="#/contatos">mais detalhes</a>
+						</div>
+					</div>
+					<div class="vitrine">
+						<div class="acessorio-img" ></div>
+						<div class="acessorio-info">
+							<h2>Tornozeleira com pingente</h2>
+							<h3>71,80 R$</h3>
+							<p>Tornozeleira 23 cm 44,00R$<br></br>pingente cacto 31,80R$</p>
+							<a href="#/contatos">mais detalhes</a>
+						</div>
+					</div>
+					<div class="vitrine">
+						<div class="acessorio-img" ></div>
+						<div class="acessorio-info">
+							<h2>Colar</h2>
+							<h3>115,00 R$</h3>
+							<p>Colar coração com micro zircônia</p>
+							<a href="#/contatos">mais detalhes</a>
+						</div>
+					</div><div class="vitrine">
+						<div class="acessorio-img" ></div>
+						<div class="acessorio-info">
+							<h2>Colar</h2>
+							<h3>95,00 R$</h3>
+							<p>colar coração</p>
+							<a href="#/contatos">mais detalhes</a>
+						</div>
+					</div>
+					<div class="vitrine">
+						<div class="acessorio-img" ></div>
+						<div class="acessorio-info">
+							<h2>Venezianas</h2>
+							<h3>149,80 R$</h3>
+							<p>veneziana 45cm 44,80.<br></br>pingente de pena 42,00<br></br>veneziana 70cm 63,00</p>
+							<a href="#/contatos">mais detalhes</a>
+						</div>
+					</div>
+					<div class="vitrine">
+						<div class="acessorio-img" ></div>
+						<div class="acessorio-info">
+							<h2>aneis</h2>
+							<h3>126,80 R$</h3>
+							<p>anel de três aros 66,90<br></br>Anel fases da lua 59,90</p>
+							<a href="#/contatos">mais detalhes</a>
+						</div>
+					</div>
+					<div class="vitrine">
+						<div class="acessorio-img" ></div>
+						<div class="acessorio-info">
+							<h2>Aneis</h2>
+							<h3>189,90 R$</h3>
+							<p>Anel de falange cinco aros 99,00 <br></br>Anel de falange coração 39,90.<br></br>Anel de coraçao vazado 51,00</p>
+							<a href="#/contatos">mais detalhes</a>
+						</div>
+					</div>
+					<div class="vitrine">
+						<div class="acessorio-img" ></div>
+						<div class="acessorio-info">
+							<h2>Colares</h2>
+							<h3>293,30 R$ R$</h3>
+							<p>veneziana 40cm 34,00R$<br></br>Pingente Sou grata 44,50R$<br></br>colar fé 89,90R$<br></br>Veneziana 70cm 63,00R$<br></br>Pingente medalha de são Bento 61,90R</p>
+							<a href="#/contatos">mais detalhes</a>
 						</div>
 					</div>
 					<div class="vitrine">
@@ -81,10 +190,65 @@ export default props =>
 						<div class="acessorio-info">
 							<h2>Anel</h2>
 							<h3>20,00 R$</h3>
-							<p>Lorem ipsum sit door is amet</p>
+							<p>Gargantilha triângulo 86,00<br></br>Veneziana 70cm 63,00<br></br>pingente sorte 46,00<br></br>pingente família 39,90<br></br>Pingente Buda 68,00</p>
 							<a href="#/contatos">mais detalhes</a>
 						</div>
 					</div>
+					<div class="vitrine">
+						<div class="acessorio-img" ></div>
+						<div class="acessorio-info">
+							<h2>Pulseira BERLOQUE</h2>
+							<h3>236,00 R$</h3>
+							<p>PULSEIRA PARA BERLOQUE CRAVEJADA CORAÇÃO EM PRATA ENVELHECIDA</p>
+							<a href="#/contatos">mais detalhes</a>
+						</div>
+					</div>
+					<div class="vitrine">
+						<div class="acessorio-img" ></div>
+						<div class="acessorio-info">
+							<h2>Pulseira BERLOQUE</h2>
+							<h3>199,00R$</h3>
+							<p>PULSEIRA PARA BERLOQUE CRAVEJADA ROLETE EM PRATA ENVELHECIDA </p>
+							<a href="#/contatos">mais detalhes</a>
+						</div>
+					</div>
+					<div class="vitrine">
+						<div class="acessorio-img" ></div>
+						<div class="acessorio-info">
+							<h2>Pulseira BERLOQUE</h2>
+							<h3>192,00 R$</h3>
+							<p>PULSEIRA PARA BERLOQUE CORAÇÃO EM PRATA ENVELHECIDA</p>
+							<a href="#/contatos">mais detalhes</a>
+						</div>
+					</div>
+					<div class="vitrine">
+						<div class="acessorio-img" ></div>
+						<div class="acessorio-info">
+							<h2>BERLOQUE</h2>
+							<h3>75,90 R$</h3>
+							<p>BERLOQUE ESMALTADO SORVETE EM PRATA ENVELHECIDA </p>
+							<a href="#/contatos">mais detalhes</a>
+						</div>
+					</div>
+					<div class="vitrine">
+						<div class="acessorio-img" ></div>
+						<div class="acessorio-info">
+							<h2>BERLOQUE</h2>
+							<h3>44,00 R$</h3>
+							<p>BERLOQUE MURANO EM PRATA 925</p>
+							<a href="#/contatos">mais detalhes</a>
+						</div>
+					</div>
+					<div class="vitrine">
+						<div class="acessorio-img" ></div>
+						<div class="acessorio-info">
+							<h2>BERLOQUE</h2>
+							<h3>44,00 R$</h3>
+							<p>BERLOQUE MURANO EM PRATA 925</p>
+							<a href="#/contatos">mais detalhes</a>
+						</div>
+					</div>
+
 				</div>
 				<div class="clear"></div>
 		    </div>
