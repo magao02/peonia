51fa5e244f0d853c8a14fd60cbc91e9cb0d281cf
Lucas Carvalho <lucas.araripina@hotmail.com>
2020-10-26 15:10:31 -0300

    atualizando o catalogo

--- src/components/catalogo.jsx
+++ src/components/catalogo.jsx
@@ -53,8 +53,8 @@ export default props =>
 						<div class="acessorio-img" ></div>
 						<div class="acessorio-info">
 							<h2>Conjunto 3 aneis</h2>
-							<h3>115,00 R$</h3>
-							<p>anel de estrelinna 59,90R$<br></br>Anel solitário com zircôniaR$<br></br>Anel de coração vazado 34,90R$</p>
+							<h3>156,80 R$</h3>
+							<p>anel de estrelinna 59,90R$<br></br>Anel solitário com zircônia 62,00R$<br></br>Anel de coração vazado 34,90R$</p>
 							<a href="#/contatos">mais detalhes</a>
 						</div>
 					</div>
@@ -79,9 +79,9 @@ export default props =>
 					<div class="vitrine">
 						<div class="acessorio-img" ></div>
 						<div class="acessorio-info">
-							<h2>Anel</h2>
-							<h3>86,00 R$</h3>
-							<p>Colar lua</p>
+							<h2>Argolinhas</h2>
+							<h3>55,00 R$</h3>
+							<p>Argolinhas</p>
 							<a href="#/contatos">mais detalhes</a>
 						</div>
 					</div>
@@ -180,16 +180,16 @@ export default props =>
 						<div class="acessorio-img" ></div>
 						<div class="acessorio-info">
 							<h2>Colares</h2>
-							<h3>293,30 R$ R$</h3>
-							<p>veneziana 40cm 34,00R$<br></br>Pingente Sou grata 44,50R$<br></br>colar fé 89,90R$<br></br>Veneziana 70cm 63,00R$<br></br>Pingente medalha de são Bento 61,90R</p>
+							<h3>194,80 R$ R$</h3>
+							<p>colar círculo 99,00<br></br>Veneziana 60 cm 64,80<br></br>Pingente Mar 31,00</p>
 							<a href="#/contatos">mais detalhes</a>
 						</div>
 					</div>
 					<div class="vitrine">
 						<div class="acessorio-img" ></div>
 						<div class="acessorio-info">
-							<h2>Anel</h2>
-							<h3>20,00 R$</h3>
+							<h2>Colares</h2>
+							<h3>302,90 R$</h3>
 							<p>Gargantilha triângulo 86,00<br></br>Veneziana 70cm 63,00<br></br>pingente sorte 46,00<br></br>pingente família 39,90<br></br>Pingente Buda 68,00</p>
 							<a href="#/contatos">mais detalhes</a>
 						</div>
@@ -244,10 +244,119 @@ export default props =>
 						<div class="acessorio-info">
 							<h2>BERLOQUE</h2>
 							<h3>44,00 R$</h3>
-							<p>BERLOQUE MURANO EM PRATA 925</p>
+							<p>berloque murano em prata 925</p>
 							<a href="#/contatos">mais detalhes</a>
 						</div>
 					</div>
+					<div class="vitrine">
+						<div class="acessorio-img" ></div>
+						<div class="acessorio-info">
+							<h2>Venezianas</h2>
+							<h3>150,60 R$</h3>
+							<p>colar de bolinhas 64,80<br></br>veneziana 40 cm 44,80<br></br>pingente estrela 41,00</p>
+							<a href="#/contatos">mais detalhes</a>
+						</div>
+					</div>
+					<div class="vitrine">
+						<div class="acessorio-img" ></div>
+						<div class="acessorio-info">
+							<h2>Brincos</h2>
+							<h3>74,00 R$</h3>
+							<p>Brinco vazado de coração 43,00<br></br>brinco âncora 31,00</p>
+							<a href="#/contatos">mais detalhes</a>
+						</div>
+					</div>
+					<div class="vitrine">
+						<div class="acessorio-img" ></div>
+						<div class="acessorio-info">
+							<h2>Pulseiras</h2>
+							<h3>121,40 R$</h3>
+							<p>pulseira coração gratidão 89,60<br></br>pulseiras de coração 31,80  </p>
+							<a href="#/contatos">mais detalhes</a>
+						</div>
+					</div>
+					<div class="vitrine">
+						<div class="acessorio-img" ></div>
+						<div class="acessorio-info">
+							<h2>Conjunto de Brincos</h2>
+							<h3>104,80,00 R$</h3>
+							<p>Brinco flor 42,00<br></br>Brinco elefante 48,80<br></br>Brinco buda 44,00</p>
+							<a href="#/contatos">mais detalhes</a>
+						</div>
+					</div>
+					<div class="vitrine">
+						<div class="acessorio-img" ></div>
+						<div class="acessorio-info">
+							<h2>Aneis</h2>
+							<h3>100,80 R$</h3>
+							<p>Anel de falange lua 44,80<br></br>Anel de falange love 36,00</p>
+							<a href="#/contatos">mais detalhes</a>
+						</div>
+					</div>
+					<div class="vitrine">
+						<div class="acessorio-img" ></div>
+						<div class="acessorio-info">
+							<h2>Pulseiras</h2>
+							<h3>146,20 R$</h3>
+							<p>pulseiras de penas 60,00<br></br>pulseira de elos 34,80<br></br>pingente Ore espere e confie 51,40</p>
+							<a href="#/contatos">mais detalhes</a>
+						</div>
+					</div>
+					<div class="vitrine">
+						<div class="acessorio-img" ></div>
+						<div class="acessorio-info">
+							<h2>Aneis</h2>
+							<h3>120,30 R$</h3>
+							<p> Anel de falange 39,90 <br></br>anel de falange trançado 35,80<br></br>Anel de falange seta 44,60</p>
+							<a href="#/contatos">mais detalhes</a>
+						</div>
+					</div>
+					<div class="vitrine">
+						<div class="acessorio-img" ></div>
+						<div class="acessorio-info">
+							<h2>Conjunto 3 aneis</h2>
+							<h3>165,00 R$</h3>
+							<p>Anel de coração vazado 47,80<br></br>Anel love 52,40<br></br>Anel três aros 64,80</p>
+							<a href="#/contatos">mais detalhes</a>
+						</div>
+					</div>
+					<div class="vitrine">
+						<div class="acessorio-img" ></div>
+						<div class="acessorio-info">
+							<h2>Conjunto 3 aneis</h2>
+							<h3>146,60 R$</h3>
+							<p>Brinco círculo vazado 48,80<br></br>Brinco de coração 48,80<br></br>Brinco cauda da Sereia 26,60 <br></br>Piercing onda do mar 22,40</p>
+							<a href="#/contatos">mais detalhes</a>
+						</div>
+					</div>
+					<div class="vitrine">
+						<div class="acessorio-img" ></div>
+						<div class="acessorio-info">
+							<h2>Colares</h2>
+							<h3>295,60 R$</h3>
+							<p>gargantilha 69,90<br></br>Colar flor de lotus 115,00<br></br>veneziana 60 cm 64,80<br></br>pingente gratidão 45,90</p>
+							<a href="#/contatos">mais detalhes</a>
+						</div>
+					</div>
+					<div class="vitrine">
+						<div class="acessorio-img" ></div>
+						<div class="acessorio-info">
+							<h2>Aneis</h2>
+							<h3>214,20 R$</h3>
+							<p>Anel de falange lua 44,80<br></br>Anel de falange love 36,00<br></br>Anel de dois aros 80,00<br></br>Anel cauda da serei 53,40</p>
+							<a href="#/contatos">mais detalhes</a>
+						</div>
+					</div>
+					<div class="vitrine">
+						<div class="acessorio-img" ></div>
+						<div class="acessorio-info">
+							<h2>Brinco Trio</h2>
+							<h3>39,90 R$</h3>
+							<p>Brinco Trio de prata 39,90</p>
+							<a href="#/contatos">mais detalhes</a>
+						</div>
+					</div>
+					
 
 				</div>
 				<div class="clear"></div>
